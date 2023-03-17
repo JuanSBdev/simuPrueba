31ed83f7d5e83c63279df789800a6765f6404f10
Juan SB <juansb.dev@gmail.com>
2023-03-17 02:23:49 -0300

    sigue

--- 06-07-08.js
+++ 06-07-08.js
@@ -11,6 +11,10 @@ function crearClasePersona() {
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
 
       // Tu código aca:
+      this.nombre = nombre,
+      this.edad = edad,
+      this.hobbies = hobbies,
+      this.amigos = amigos;
 
     }
 
@@ -18,8 +22,8 @@ function crearClasePersona() {
       // El método 'addFriend' recibe un string 'nombre' y un entero 'edad' y debe agregar un objeto:
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // No debe retornar nada.
-
       // Tu código aca:
+      amigos.push({nombre, edad})
 
     }
 
@@ -28,7 +32,7 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
-
+      hobbies.push(hobby);
     }
     getFriends() {
       // El método 'getFriends' debe retornar un arreglo con sólo los nombres del arreglo de amigos
@@ -38,6 +42,11 @@ function crearClasePersona() {
       // persona.getFriends() debería devolver ['martin', 'toni']
 
       // Tu código aca:
+      let niu = amigos.map(function(x){
+        return x['nombre'];
+      });
+
+return niu;
 
     }
 
@@ -47,7 +56,8 @@ function crearClasePersona() {
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
 
       // Tu código aca:
-
+      return this.hobbies;
+        
     }
 
     getPromedioEdad() {
@@ -66,7 +76,12 @@ function crearClasePersona() {
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
       // Tu código aca:
-
+      let niu = 0; 
+      const amigos = this.amigos;
+      for (let i = 0; i < amigos.length; i++) {
+        niu += amigos[i].edad
+      }
+      return niu/amigos.length;
     }
   };
 
